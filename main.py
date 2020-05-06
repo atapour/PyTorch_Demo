8fa99e986355e68d3d22f27ec0b4a0d4536655df
amir <atapour@users.noreply.github.com>
2020-05-06 09:42:33 +0100

    small changes for consistency

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
     
-print('Using PyTorch version:', torch.__version__, ' Device:', device)
+print('PyTorch version:', torch.__version__, ' Device:', device)
+# --------------------------------
 
+# --------------------------------
+# Hyper-Parameters
+learning_rate = 0.001 # Initial learning rate
+training_epochs = 15 # Number of epochs to train
+batch_size = 100 # Number of images per batch
+display_step = 1 # How often to output model metrics during training
 
-batch_size = 100
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+# --------------------------------
 
+# --------------------------------
+# The dataset
 train_dataset = datasets.MNIST('./data', 
                                train=True, 
                                download=True, 
@@ -32,16 +46,19 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
                                                 batch_size=batch_size, 
                                                 shuffle=False)
+# --------------------------------
 
 
 
 
-class Net(nn.Module):
+class Multilayer_Perceptron(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(28*28, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 10)
+        super(Multilayer_Perceptron, self).__init__()
+
+        # Hidden fully connected layer with 256 neurons
+        self.fc1 = nn.Linear(n_input, n_hidden_1)
+        self.fc2 = nn.Linear(n_hidden_1, n_hidden_2)
+        self.fc3 = nn.Linear(n_hidden_2, n_classes)
 
     def forward(self, x):
         x = x.view(-1, 28*28)
@@ -49,41 +66,43 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         return F.log_softmax(self.fc3(x), dim=1)
 
-model = Net().to(device)
+# defining model, optimizer and loss:
+model = Multilayer_Perceptron().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
 
-print(model)
-
-def train(epoch, log_interval=200):
+# training function:
+def train():
     # Set model to training mode
     model.train()
     
+    for epoch in range(training_epochs):
     # Loop over each batch from the training set
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # Copy data to GPU if needed
-        data = data.to(device)
-        target = target.to(device)
+        for batch_idx, (img, lbl) in enumerate(train_loader):
+            # Copy image data to GPU if needed
+            img = img.to(device)
+            lbl = lbl.to(device)
 
-        # Zero gradient buffers
-        optimizer.zero_grad() 
-        
-        # Pass data through the network
-        output = model(data)
+            # Zero gradient buffers
+            optimizer.zero_grad() 
+            
+            # Pass image data through the network
+            output = model(img)
 
-        # Calculate loss
-        loss = criterion(output, target)
+            # Calculate loss
+            loss = criterion(output, lbl)
 
-        # Backpropagate
-        loss.backward()
+            # Backpropagate
+            loss.backward()
+            
+            # Update weights
+            optimizer.step()
         
-        # Update weights
-        optimizer.step()
-        
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data.item()))
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(loss.item()))
+            # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            #     epoch, batch_idx * len(data), len(train_loader.dataset),
+            #     100. * batch_idx / len(train_loader), loss.data.item()))
 
 
 def validate(loss_vector, accuracy_vector):
@@ -106,9 +125,7 @@ def validate(loss_vector, accuracy_vector):
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         val_loss, correct, len(test_loader.dataset), accuracy))
 
-epochs = 10
 
 lossv, accv = [], []
-for epoch in range(1, epochs + 1):
-    train(epoch)
-    validate(lossv, accv)
+train()
+validate(lossv, accv)
