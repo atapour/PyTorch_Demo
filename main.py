127203094787a4d604453c9acfdacafc6691530c
amir <atapour@users.noreply.github.com>
2020-05-06 09:23:27 +0100

    first draft of the script

---
+++ main.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+    
+print('Using PyTorch version:', torch.__version__, ' Device:', device)
+
+
+batch_size = 100
+
+train_dataset = datasets.MNIST('./data', 
+                               train=True, 
+                               download=True, 
+                               transform=transforms.ToTensor())
+
+test_dataset = datasets.MNIST('./data', 
+                                    train=False, 
+                                    transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                                batch_size=batch_size, 
+                                                shuffle=False)
+
+
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28*28, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return F.log_softmax(self.fc3(x), dim=1)
+
+model = Net().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()
+
+print(model)
+
+def train(epoch, log_interval=200):
+    # Set model to training mode
+    model.train()
+    
+    # Loop over each batch from the training set
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Copy data to GPU if needed
+        data = data.to(device)
+        target = target.to(device)
+
+        # Zero gradient buffers
+        optimizer.zero_grad() 
+        
+        # Pass data through the network
+        output = model(data)
+
+        # Calculate loss
+        loss = criterion(output, target)
+
+        # Backpropagate
+        loss.backward()
+        
+        # Update weights
+        optimizer.step()
+        
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data.item()))
+
+
+def validate(loss_vector, accuracy_vector):
+    model.eval()
+    val_loss, correct = 0, 0
+    for data, target in test_loader:
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data)
+        val_loss += criterion(output, target).data.item()
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    val_loss /= len(test_loader)
+    loss_vector.append(val_loss)
+
+    accuracy = 100. * correct.to(torch.float32) / len(test_loader.dataset)
+    accuracy_vector.append(accuracy)
+    
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(test_loader.dataset), accuracy))
+
+epochs = 10
+
+lossv, accv = [], []
+for epoch in range(1, epochs + 1):
+    train(epoch)
+    validate(lossv, accv)
